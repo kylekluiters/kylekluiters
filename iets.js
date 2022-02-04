86211f59a4d377e9db78cf78456ed804109afc92
kylekluiters <9016673@student.zadkine.nl>
2022-02-04 12:03:01 +0100

    knor knor

--- iets.js
+++ iets.js
@@ -1,3 +1,4 @@
+/*
 // var age = 18;
 //var niet gebruiken
 let age2 = 20;
@@ -9,3 +10,26 @@ const age3 = 40;
 
 console.log(age3);
 
+*/
+//string
+let idk = "knor knor ";
+let idk2 = 'het gaat "werken"';
+
+//number
+let afstand = 5473.987656;
+let afstand2 = 5e10;
+
+//booleans
+let x = 20;
+let y = 30;
+let z = 20;
+let answer1 = (x == y);
+let answer2 = (x == z);
+
+
+console.log(answer1)
+console.log(answer2)
+console.log(afstand)
+console.log(afstand2)
+console.log(idk)
+console.log(idk2)
